d92f7dfa1a52ecaf4b29866a2e544070c6a099e0
AnhLe <le.quang.anh@styl.solutions>
2026-07-18 18:02:08 +0700

    update setup.md

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+# Install claude yocto-agent-skills
+# git clone https://github.com/Higangssh/yocto-agent-skills ~/.claude/skills/yocto
+
 cd .tmp
 # git clone https://git.openembedded.org/bitbake
-
 git clone https://git.yoctoproject.org/poky
 cd poky
 git checkout scarthgap
